c7e5365f94f8226fb696c8314c58054edc368493
unknown <atiagosoares@gmail.com>
2020-03-22 20:51:11 -0300

    funcoes andré prontas

--- find_larger_economies.R
+++ find_larger_economies.R
@@ -29,8 +29,49 @@ get_biggest_economies <- function(c_indicators = country_indicators, c_ind_serie
   }
   return(economies)
 }
+
+
+get_mean_exports <- function(countrylist,
+                 c = countries,
+                 c_rel = country_relationships,
+                 c_ind = country_indicators,
+                 rel_ind = relationship_indicators, 
+                 c_ind_series = country_indicator_series, 
+                 rel_ind_series = relationship_indicator_series
+){
   
+  summ <- data.frame(
+    'country' = character(1),
+    'mean_exports' = double(1),
+    'sd_exports' = double(1),
+    stringsAsFactors = FALSE
+  )
+  #encontrar id do indicador
+  ind_id = lookup('All Products Export Exports (in US$ Mil)', rel_ind[['relationship_indicator_name']], rel_ind[['relationship_indicator_id']])
+  #econtrar id do mundo
+  wld_id = lookup('World', c[['country_name']], c[['country_id']])
   
+  for(i in countrylist){
+    cname = lookup(i, c[['country_id']], c[['country_name']])
+    #econtrar relacionamento
+    rel_id <- c_rel[c_rel[['country1_id']] == i,]
+    rel_id <- lookup(wld_id, rel_id[['country2_id']], rel_id[['relationship_id']])
+    #puxar dados relevantes
+    ss <- rel_ind_series[(rel_ind_series[['relationship_id']] == rel_id)&(rel_ind_series[['relationship_indicator_id']] == ind_id),]
+    summ <- rbind(summ,list( cname, mean(ss[['data_value']], na.rm = TRUE), sd(ss[['data_value']], na.rm = TRUE)))
+
+  }
+  summ <- summ[-c(1),]
+  return(summ)
+  }  
+  
+
+write.csv(countries, file = 'countries.csv', sep = ',', dec = '.')
+write.csv(country_relationships, file = 'country_relationships.csv', sep = ',', dec = '.')
+write.csv(country_indicators, file = 'country_indicators.csv', sep = ',', dec = '.')
+write.csv(relationship_indicators, file = 'relationship_indicators.csv', sep = ',', dec = '.')
+write.csv(country_indicator_series, file = 'country_indicator_series.csv', sep = ',', dec = '.')
+write.csv(relationship_indicator_series, file = 'relationship_indicator_serires.csv', sep = ',', dec = '.')
 
 
 library(xlsx)
