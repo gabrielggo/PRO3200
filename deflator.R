6070d12454387c4fc6f23c81f6233687497798ec
unknown <atiagosoares@gmail.com>
2020-05-23 15:09:40 -0300

    deflator

--- deflator.R
+++ deflator.R
@@ -1,6 +1,6 @@
 setwd('F:/Google Drive/Privado/Faculdade/Estatística 2020/PRO3200')
 
-trade = read.csv('wits_en_trade_summary_allcountries_allyears/en_USA_AllYears_WITS_Trade_Summary.CSV', header = TRUE, sep = ',', dec = '.', stringsAsFactors = FALSE)
+trade = read.csv('wits_en_trade_summary_allcountries_allyears/en_Wld_AllYears_WITS_Trade_Summary.CSV', header = TRUE, sep = ',', dec = '.', stringsAsFactors = FALSE)
 deflator_table = read.csv('API_NY.GDP.DEFL.ZS_DS2_en_csv_v2_1070444.csv', header = TRUE, sep = ',', dec = '.', stringsAsFactors = FALSE)
 
 deflator <- function(t, d){
@@ -8,6 +8,7 @@ deflator <- function(t, d){
   reporter = t[1 , 1]
 
     dv <- d[d[1] == reporter, 33:63]
+    print(dv)
 
   #A próxima parte existe para corrigir os 'buracos'com valores NA 
   NonNAindex = which(!is.na(dv[1,]))
@@ -36,3 +37,4 @@ deflator <- function(t, d){
   
   return(t)
 }
+
