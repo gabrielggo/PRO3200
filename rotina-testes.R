2c816d6e1a8910ab214a52a803238c6d15795bc1
unknown <atiagosoares@gmail.com>
2020-07-03 20:20:36 -0300

    finalizaçao da segunda parte da entrega final

--- rotina-testes.R
+++ rotina-testes.R
@@ -1,3 +1,7 @@
+#FUNÇOES NECESSÁRIAS
+
+
+
 files <- list.files('dados_1afase', full.names = TRUE)
 a = c('wits_en_trade_summary_allcountries_allyears/en_USA_AllYears_WITS_Trade_Summary.CSV',
       'wits_en_trade_summary_allcountries_allyears/en_CHN_AllYears_WITS_Trade_Summary.CSV',
@@ -7,32 +11,112 @@ a = c('wits_en_trade_summary_allcountries_allyears/en_USA_AllYears_WITS_Trade_Su
       )
 
 
-#Ajustar dados dos países
+#Cotelar os dados da fonte
 
-pais = read.csv('wits_en_trade_summary_allcountries_allyears/en_CHN_AllYears_WITS_Trade_Summary.CSV',
+chn_source = read.csv('wits_en_trade_summary_allcountries_allyears/en_CHN_AllYears_WITS_Trade_Summary.CSV',
+                header = TRUE, sep = ',', dec = '.', stringsAsFactors = FALSE)
+usa_source = read.csv('wits_en_trade_summary_allcountries_allyears/en_USA_AllYears_WITS_Trade_Summary.CSV',
+                header = TRUE, sep = ',', dec = '.', stringsAsFactors = FALSE)
+deu_source = read.csv('wits_en_trade_summary_allcountries_allyears/en_DEU_AllYears_WITS_Trade_Summary.CSV',
+                header = TRUE, sep = ',', dec = '.', stringsAsFactors = FALSE)
+jpn_source = read.csv('wits_en_trade_summary_allcountries_allyears/en_JPN_AllYears_WITS_Trade_Summary.CSV',
+                header = TRUE, sep = ',', dec = '.', stringsAsFactors = FALSE)
+fra_source = read.csv('wits_en_trade_summary_allcountries_allyears/en_FRA_AllYears_WITS_Trade_Summary.CSV',
                 header = TRUE, sep = ',', dec = '.', stringsAsFactors = FALSE)
 
-exp_absolutas = getExportacoesPaises(pais)
-exp_relativas = getProductshare(pais)
+#Recortes necessários
+usa_abs = getExportacoesPaises(usa_source)
+usa_rel= getProductshare(usa_source)
 
-exp_absolutas = deflator(exp_absolutas) #Deflaciona os dados absolutos
+chn_abs = getExportacoesPaises(chn_source)
+chn_rel= getProductshare(chn_source)
 
-exp_absolutas = agrega_menores_setores(exp_absolutas)
-exp_relativas = agrega_menores_setores(exp_relativas)
+jpn_abs = getExportacoesPaises(jpn_source)
+jpn_rel= getProductshare(jpn_source)
 
-exp_crescimento = crescimentoAnual(exp_absolutas)
+deu_abs = getExportacoesPaises(deu_source)
+deu_rel= getProductshare(deu_source)
+
+fra_abs = getExportacoesPaises(fra_source)
+fra_rel= getProductshare(fra_source)
+
+
+#deflaciona os dados absolutos
+usa_abs = deflator(usa_abs)
+chn_abs = deflator(chn_abs)
+jpn_abs = deflator(jpn_abs)
+deu_abs = deflator(deu_abs)
+fra_abs = deflator(fra_abs)
+
+#junta os menores setores
+usa_abs = agrega_menores_setores(usa_abs)
+usa_rel = agrega_menores_setores(usa_rel)
+
+chn_abs = agrega_menores_setores(chn_abs)
+chn_rel = agrega_menores_setores(chn_rel)
+
+jpn_abs = agrega_menores_setores(jpn_abs)
+jpn_rel = agrega_menores_setores(jpn_rel)
+
+deu_abs = agrega_menores_setores(deu_abs)
+deu_rel = agrega_menores_setores(deu_rel)
+
+fra_abs = agrega_menores_setores(fra_abs)
+fra_rel = agrega_menores_setores(fra_rel)
+
+#calcula crescimento anual(dados absolutos)
+usa_cres = crescimentoAnual(usa_abs)
+chn_cres = crescimentoAnual(chn_abs)
+jpn_cres = crescimentoAnual(jpn_abs)
+deu_cres = crescimentoAnual(deu_abs)
+fra_cres = crescimentoAnual(fra_abs)
+
+
+#PRIMEIRA ANÁLISE: REGRESSÃO LINEAR DESVIO PADRÃO DO PRODUCT EXPORT SHARE
+#MOTIVO: SE HOUVER MUDANÇA NO DESVIO PADRÃO PODEMOS AFIRMAR QUE A COMPOSIÇÃO MUDOU
+anos = c(2018:1988)
+
+desvios = c()
+  
+for(j in 6:36){
+  desvios[j - 5] <- sd(fra_rel[,j])  #alterar 'usa' pela sigla do pais que queremos analisar
+}
+  
+plot(x = anos, y = desvios, type = 'b') #TODO: Melhorar a estética desse gráfico para colocar no relatório
+  
+teste1 = lm(desvios~anos)
+  
+summary(teste1) #Resultado: podemos afirmar mudança da composição de exportação de todos países menos França
+
+#SEGUNDA ANÁLISE: REGRESSÃO LINEAR DO PRODUCT SHARE DE TODOS SETORES
+#MOTIVO: JÁ IDENTIFICAMOS QUE A COMPOSIÇÃO MUDOU, AGORA VAMOS IDENTIFICAR QUAIS SETORES MUDARAM
+
+exp_rel = rbind(usa_rel, chn_rel, jpn_rel, deu_rel)
 
 
 
-
-#Desvio padrão do crescimento dos setores ao longo do ano
-desvios <- c()
-
-for(i in 6:35){
-  desvios[i - 5] <- sd(exp_relativas[,i])
+for(i in 1:nrow(usa_rel)){
+  
+  str = usa_rel[i, 3]
+  
+  productShare = c(usa_rel[i, 6:36], chn_rel[i, 6:36], jpn_rel[i, 6:36], deu_rel[i, 6:36])
+  anos4 = c(anos, anos, anos, anos)
+  
+  plot(y = productShare,
+       x = anos4,
+       main = str)
+  
+  print(str)
+  
+  teste2 = lm(as.numeric(productShare)~anos4)
+  
+  print(summary(teste2))
 }
 
-plot(x = c(2018:1989), y = desvios)
+
+
+
+plot(x = c(2018:1989), y = desvios, type = 'b')
 
 cor.test(desvios, c(2018:1989)) #p-valor 2.006
 
